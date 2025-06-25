bd02c9ac432f0ae8a78c714607a80dc7fd2189ac
thielang <xuanthuc123412@gmail.com>
2025-06-25 21:00:50 +0700

    fea/do-intro-header

---
+++ src/assets.js
@@ -0,0 +1,12 @@
+const dataLookbook = [
+    {
+        id: 1,
+        img: "./imgs/img_lookbook1.webp",
+        title: "BEGIN WITH THE SUIT"
+    },
+    {
+        id: 1,
+        img: "./imgs/img_lookbook1.webp",
+        title: "BEGIN WITH THE SUIT"
+    }
+]
